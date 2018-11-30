ff5365f11606ab964485fff31fc6d07b498587b7
wangkai <m17713277770@163.com>
2018-11-30 07:46:26 +0000

    修改中间件注释代码

--- app/Http/Middleware/LoginMiddleware.php
+++ app/Http/Middleware/LoginMiddleware.php
@@ -31,22 +31,6 @@ class LoginMiddleware
             return redirect('/admin/resources/users/'.auth()->id().'/edit');
         }
 
-//        $id = auth()->user()->create_work;
-//
-//        if ( $id && str_is('/nova-api/comments/*', $request->getRequestUri())) {
-//
-//            return redirect('/nova-api/works/'.$id);
-//        }
-////
-//        if ($id &&
-//            str_is('/nova-api/comments?search=&filters=&orderBy=&orderByDirection=desc&perPage=25&trashed=&page=1&viaResource=comments&viaResourceId=*', $request->getRequestUri())
-//        ) {
-//            auth()->user()->create_work = 0;
-//            auth()->user()->save();
-//
-//            return redirect('/nova-api/comments?search=&filters=&orderBy=&orderByDirection=desc&perPage=25&trashed=&page=1&viaResource=works&viaResourceId='.$id.'&viaRelationship=comments&relationshipType=hasMany');
-//        }
-
         return $next($request);
     }
 }
